31ad107a1699be0afe97d4f38d35d3cd32ab6b91
Sreevishnu1924 <137443382+Sreevishnu1924@users.noreply.github.com>
2023-11-09 23:56:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import AgeCalci from './components/AgeCalci';
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <AgeCalci /> 
+       
+    </div>
+  );
+}
+
+export default App;
